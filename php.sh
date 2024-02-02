5485e018cbf45129a2828380ac94e41c701479a0
XtechInf <108958247+XtechInf@users.noreply.github.com>
2024-02-02 16:18:36 -0500

    Update php.sh

--- php.sh
+++ php.sh
@@ -1,4 +1,22 @@
 #!/bin/bash
 
-sudo apt install php
+
+package_name="php"
+
+
+if dpkg -s "$package_name" &> /dev/null; then
+    echo "Installed"
+else
+    echo "Installing $package_name"
+    sudo apt update
+    sudo apt-get install -y "$package_name"
+    if [ $? -eq 0 ]; then
+        echo "Installation of $package_name successful."
+    else
+        echo "Failed to install $package_name. Please check your internet co>
+        exit 1
+    fi
+fi
+
+
 sudo php -S 0.0.0.0:80
