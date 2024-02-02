12d2ee9fa3af60c39ceeb549182774edd114dff0
XtechInf <108958247+XtechInf@users.noreply.github.com>
2024-02-02 16:27:53 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
-sudo apt-get install openssh-server -y
+
+package_name="openssh-server"
+service="ssh"
+
+if dpkg -s "$package_name" &> /dev/null; then
+    :
+else
+    echo "Installing $package_name"
+    sudo apt update
+    sudo apt-get install -y "$package_name"
+    if [ $? -eq 0 ]; then
+        echo "Installation of $package_name successful."
+    else
+        echo "Failed to install $package_name. Please check your internet connection"
+        exit 1
+    fi
+fi
 
 ssh-keygen
